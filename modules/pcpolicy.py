884aa23970f5a1aa7c1020d74c45b3be684a565b
Erick Moore <erick.moore@gmail.com>
2024-11-27 10:43:47 -0500

    feat: use filter function and options

--- modules/pcpolicy.py
+++ modules/pcpolicy.py
@@ -2,38 +2,40 @@
 
 import pandas as pd
 import click
+from options import add_dynamic_options
 from modules.config import url, password, username
 from modules.api import login, get_policies, apply_policies, get_compliance
 from modules.messages import print_status, print_results, print_total, print_apply
-from modules.arg_validator import MutuallyExclusiveOption, SeverityType
 from modules.export import export_csv
+from modules.filter_data import filter_column
 from datetime import datetime
 import json
 
 @click.command()
-@click.option('--apply', is_flag=True, help="Apply selected changes")
-@click.option('--severity', type=SeverityType(), help=f"Policy severity, accepts: c: critical, h: high, m: medium, l: low, i: informational)")
-@click.option('--new-severity', type=click.Choice(['critical', 'high', 'medium', 'low', 'informational']), help="Change selected policy severity")
-@click.option('--policy-subtype', type=click.Choice(['run', 'build', 'run_and_build', 'audit', 'data_classification', 'dns', 'malware', 'network_event', 'network', 'ueba', 'permissions', 'identity']))
-@click.option('--cloud', type=click.Choice(['aws', 'azure', 'gcp', 'alibaba', 'oci']))
-@click.option('--policy-enabled', is_flag=True, help="Find enabled policies")
-@click.option('--policy-disabled', is_flag=True, help="Find disabled policies")
-@click.option('--enable', is_flag=True, cls=MutuallyExclusiveOption, mutually_exclusive=["disable"], help="Enable selected policies")
-@click.option('--disable', is_flag=True, cls=MutuallyExclusiveOption, mutually_exclusive=["enable"], help="Disable selected policies")
-@click.option('--include', multiple=True, type=str, help="Include policies by name")
-@click.option('--exclude', multiple=True, type=str, help="Exclude policies by name")
-@click.option('--match', type=click.Choice(['any', 'all']))
-@click.option('--list-compliance', is_flag=True, help="List compliance names")
-@click.option('--include-label', type=str, help="Include policies with matching label name")
-@click.option('--exclude-label', multiple=True, type=str, help="Exclude policies with matching label name")
-@click.option('--new-label', type=str, help="Add a label to matched policies")
-@click.option('--remove-label', type=str, help="Remove label to matched policies")
-@click.option('--compliance', type=str, help="Match policies against a compliance standard")
-@click.option('--export', is_flag=True, cls=MutuallyExclusiveOption, mutually_exclusive=["apply"], help="Export results as a CSV")
+@add_dynamic_options
 
-def main(apply, severity, policy_subtype, cloud, policy_enabled, policy_disabled, enable, disable, include, exclude, new_severity, list_compliance, include_label, new_label, exclude_label, remove_label, compliance, export, match):
-    
+def main(**kwargs):
     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    
+    apply           = kwargs.get('apply', False)
+    cloud           = kwargs.get('cloud')
+    compliance      = kwargs.get('compliance')
+    disable         = kwargs.get('disable', False)
+    enable          = kwargs.get('enable', False)
+    exclude         = kwargs.get('exclude')
+    exclude_label   = kwargs.get('exclude_label')
+    export          = kwargs.get('export', False)
+    include         = kwargs.get('include')
+    include_label   = kwargs.get('include_label')
+    list_compliance = kwargs.get('list_compliance', False)
+    matchall        = kwargs.get('matchall', False)
+    new_label       = kwargs.get('new_label')
+    new_severity    = kwargs.get('new_severity')
+    policy_disabled = kwargs.get('policy_disabled')
+    policy_enabled  = kwargs.get('policy_enabled')
+    policy_subtype  = kwargs.get('policy_subtype')
+    remove_label    = kwargs.get('remove_label')
+    severity        = kwargs.get('severity')
         
     policy_status = None
     if policy_enabled: policy_status  = 'true'
@@ -43,18 +45,17 @@ def main(apply, severity, policy_subtype, cloud, policy_enabled, policy_disabled
     if enable: policy_action  = 'enable'
     if disable: policy_action = 'disable'
     
-    if not match:
-        match = 'any'
-    
+    match_function = all if matchall == True else any
+
     token = login(url, username, password)
     
     if list_compliance:
         compliance_standards = get_compliance(url, token)
         df = pd.DataFrame(compliance_standards)
         if include:
-            df = df[df['name'].apply(lambda x: any(f in x for f in include))]
+            df = filter_column(df, 'name', include, match_function)
         if exclude:
-            df = df[~df['name'].apply(lambda x: any(f in x for f in exclude))]
+            df = filter_column(df, 'name', exclude, match_function, exclude=True)
         for index, row in df.iterrows():
             compliance_name = row['name']
             print(compliance_name)
@@ -64,32 +65,17 @@ def main(apply, severity, policy_subtype, cloud, policy_enabled, policy_disabled
     
     # Create Pandas DataFrame
     df = pd.DataFrame(policies)
-    
-    # column_names = df.columns.tolist()
-    # print(column_names)
-    
-    # Filter DataFrame for policies that match applied filters
+
+    # Filter data
     if include:
-        include_lower = [f.lower() for f in include]  # Normalize include filters
-        if match == "all":
-            df = df[df['name'].str.lower().apply(lambda x: all(f in x for f in include_lower))]
-        if match == "any":
-            df = df[df['name'].str.lower().apply(lambda x: any(f in x for f in include_lower))]            
+        df = filter_column(df, 'name', include, match_function)
     if exclude:
-        exclude_lower = [f.lower() for f in exclude]
-        if match == "all":
-            df = df[~df['name'].str.lower().apply(lambda x: all(f.lower() in x for f in exclude_lower))]
-        if match == "any":
-            df = df[~df['name'].str.lower().apply(lambda x: any(f.lower() in x for f in exclude_lower))]
+        df = filter_column(df, 'name', exclude, match_function, exclude=True)
     if include_label:
-        df = df[df['labels'].apply(lambda labels: 
-        any(any(f.lower() in label.lower() for f in include_label) for label in labels)
-    )]        
+        df = filter_column(df, 'labels', include_label, match_function)
     if exclude_label:
-        df = df[~df['labels'].apply(lambda labels: 
-        any(any(f.lower() in label.lower() for f in exclude_label) for label in labels)
-    )]
-    
+        df = filter_column(df, 'labels', exclude_label, match_function, exclude=True)
+
     # Set policy count to zero before parsing data
     total_count     = 0
     enabled_count   = 0
@@ -108,7 +94,7 @@ def main(apply, severity, policy_subtype, cloud, policy_enabled, policy_disabled
         new_labels  = []
         last_label  = False
         
-        if compliance is None or (compliance_data is not None and isinstance(compliance_data, list) and any(item.get('standardName') == compliance for item in compliance_data)):
+        if compliance is None or (compliance_data is not None and isinstance(compliance_data, list) and match_function(item.get('standardName') == compliance for item in compliance_data)):
 
             if policy_status == True:
                 enabled_count += 1
